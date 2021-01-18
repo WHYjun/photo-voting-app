4b8aa7e291dad1dd93b62b1a5c9410308b9acb23
Young Jun Choi <yjunchoi93@gmail.com>
2021-01-18 11:21:02 -0600

    Change to createPool to fix connection issue in ClearDB

--- connection.js
+++ connection.js
@@ -3,17 +3,11 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-});
-
-connection.connect((err) => {
-  if (err) {
-    throw err;
-  }
 });
 
 module.exports = connection;
